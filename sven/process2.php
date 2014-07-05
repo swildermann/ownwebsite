ad5860d9119f3442de4a645893e71543023ef49e
swildermann <git@wildermannonline.de>
2014-07-05 19:39:22 +0200

    added the from

--- sven/process2.php
+++ sven/process2.php
@@ -5,7 +5,7 @@
     $sub = stripslashes($sub);
     $text = stripslashes($text);
     $receiver = stripslashes($receiver);
-    mail("$receiver","$sub","$text","anonym");
+    mail("$receiver","$sub","$text"," From: anonym");
     ?> , Deine Mail wurde versandt.
 
 </html>
