83ce50fc19befd6c6dc430df6d1664b0ae8ed168
swildermann <git@wildermannonline.de>
2014-07-05 19:43:34 +0200

    more comfort

--- sven/process2.php
+++ sven/process2.php
@@ -4,8 +4,10 @@
     $text = stripslashes($text);
     $sub = stripslashes($sub);
     $text = stripslashes($text);
+    $sender = stripslashes($sender)
+    $name = stripslashes($name)
     $receiver = stripslashes($receiver);
-    mail("$receiver","$sub","$text","From: anonym <anonym@anonym.com>");
+    mail("$receiver","$sub","$text","From: $name $sender);
     ?> , Deine Mail wurde versandt.
 
 </html>
