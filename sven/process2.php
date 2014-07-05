1b98d3a7b0f497a180d483f73afbe1fe6c0a799a
swildermann <git@wildermannonline.de>
2014-07-05 19:36:02 +0200

    testing

--- sven/process2.php
+++ sven/process2.php
@@ -2,8 +2,10 @@
 <?php
     @extract($_POST);
     $text = stripslashes($text);
+    $sub = stripslashes($sub)
+    $text = stripslashes($text)
     $receiver = stripslashes($receiver);
-    mail("$receiver","","","");
+    mail("$receiver","$sub","$text","anonym");
     ?> , Deine Mail wurde versandt.
 
 </html>
