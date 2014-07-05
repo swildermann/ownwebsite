ffea39654bef47a6ccf29343da01c9fbf7c18226
swildermann <git@wildermannonline.de>
2014-07-05 19:46:34 +0200

    try to fix a bug

--- sven/process2.php
+++ sven/process2.php
@@ -3,11 +3,10 @@
     @extract($_POST);
     $text = stripslashes($text);
     $sub = stripslashes($sub);
-    $text = stripslashes($text);
     $sender = stripslashes($sender);
     $name = stripslashes($name);
     $receiver = stripslashes($receiver);
-    mail("$receiver","$sub","$text","From: $name $sender);
+    mail("$receiver","$sub","$text","From: $name <$sender>);
     ?> , Deine Mail wurde versandt.
 
 </html>
