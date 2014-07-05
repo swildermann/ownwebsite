24c436fc43440cabd348ad568de2efeb6bb1acbf
swildermann <git@wildermannonline.de>
2014-07-05 19:29:04 +0200

    bugs

--- sven/process2.php
+++ sven/process2.php
@@ -1,14 +1,7 @@
 <html>
 <?php
-    @extract($_POST);
-    $sub= stripslashes($sub);
-    $name = stripslashes($name);
-    $sender = stripslashes($sender);
-    $receiver = stripslashes($receiver);
-    $text = stripslashes($text);
-    mail('test@wildermann.berlin',$sub,"$name $text","From: $name <admin@your-domain.com>");
-    mail('test@wildermann.berlin',"$sub","$text","From: $name <$sender>);
-    echo stripslashes($name);
+    mail('test@wildermann.berlin',"","","");
+
     ?> , Deine Mail wurde versandt.
 
 </html>
