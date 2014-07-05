d714b8ec728263b2b2338964fe2f5935367699a5
swildermann <git@wildermannonline.de>
2014-07-05 19:22:56 +0200

    bug fixing

--- sven/process.php
+++ sven/process.php
@@ -6,6 +6,7 @@
     $sender = stripslashes(§sender);
     $receiver = stripslashes(§receiver);
     $text = stripslashes($text);
+    mail('test@wildermann.berlin',$sub,"$name $text","From: $name <admin@your-domain.com>");
     mail('test@wildermann.berlin',"$sub","$text","From: $name <$sender>);
     echo stripslashes($name);
     ?> , Deine Mail wurde versandt.
