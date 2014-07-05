f7a935bfa1ed1dca3602663f287b494a9a279b24
swildermann <git@wildermannonline.de>
2014-07-05 19:03:13 +0200

    changed receiver

--- sven/process.php
+++ sven/process.php
@@ -4,7 +4,7 @@
     $sub="Form feedback";
     $name = stripslashes($name);
     $email = stripslashes($email);
-    mail('admin@your-domain.com',$sub,"$name $email","From: $name <admin@your-domain.com>");
+    mail('test@wildermann.berlin',$sub,"$name $email","From: $name <admin@your-domain.com>");
     echo stripslashes($name);
     ?> , we will drop you a line shortly.
 
