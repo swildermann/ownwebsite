e97388bd405768ffccda91f21a2cf9155ebdf9f3
swildermann <git@wildermannonline.de>
2015-08-08 19:50:06 +0200

    some small changes

--- dev_steph/mail/contact_me.php
+++ dev_steph/mail/contact_me.php
@@ -16,8 +16,8 @@ $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'test@wildermannonline.de'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Kontakt durch Kontaktformular  $name";
+$email_body = "Du hast eine Nachricht durch dein Kontaktformular auf www.wildermannonline.de erhalten. \n\n"."Das sind die Details: :\n\nName: $name\n\nE-Mail: $email_address\n\nTelefon: $phone\n\nNachricht:\n$message";
 $headers = "From: noreply@wildermannonline.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
